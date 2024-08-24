3ca2c293db5c9b669da96f94445d7cd72dc361cc
junnyyummy <2214054132@qq.com>
2024-08-25 06:55:17 +0800

    test push code with vscode

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@ int main(void){
     int b=2;
     int c;
     c=findmax(a,b);
+
     printf("Hello World!  %d\n",c);
     printf("Hello Minus folder!  %d\n",minus(10,b));
     printf("COMPARE_A_B!  %d\n",compareab(10,c));
+    
     return 0;
 }
